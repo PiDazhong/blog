522e75c42225b75466ab98b3ecd0117451f01c6d
PiDazhong <185431334@qq.com>
2020-10-21 00:12:03 +0800

    deploy

---
+++ assets/js/constants.js
@@ -0,0 +1,10 @@
+const CONSTANTS = {
+    TEL: '10086',
+    E_MAIL: 'pidazhong@whu.edu.cn',
+    WE_CHAT:'pdz_wechat',
+    PINK: '#e83e8c',
+    BLUE: '#0f9de8',
+    PURPLE: '#781ee8'
+}
+
+export default CONSTANTS
